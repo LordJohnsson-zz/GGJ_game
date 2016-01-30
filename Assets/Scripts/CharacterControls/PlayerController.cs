054a811ba212ea4e340dcf41ce168bef8e6e6de4
Lauri Haukijärvi <lauri@lauhamedia.fi>
2016-01-30 13:25:49 +0200

    PlayerController & Triggers

---
+++ Assets/Scripts/CharacterControls/PlayerController.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+    [Range(0, 1)]
+    public float playerSpeed;
+
+    PolygonCollider2D walkableArea;
+    Trigger targetTrigger;
+    Vector3 targetPos;
+    bool facingRight;
+
+    void Awake()
+    {
+        walkableArea = GameObject.Find("WalkableArea").GetComponent<PolygonCollider2D>();
+    }
+
+    void Start () {
+
+        Transform startNode = null;
+        if (GameObject.FindGameObjectWithTag("StartNode") != null)
+        {
+            if (GameObject.FindGameObjectWithTag("StartNode").transform != null)
+            {
+                startNode = GameObject.FindGameObjectWithTag("StartNode").transform;
+            }
+
+            if (startNode != null)
+            {
+                transform.position = startNode.position;
+            }
+        }
+        targetPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mouseDownPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseDownPos.z = transform.position.z;
+
+            RaycastHit2D hit = Physics2D.Raycast(mouseDownPos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.tag == "Trigger")
+                {
+                    Transform[] walkTargets = hit.collider.GetComponentsInChildren<Transform>();
+                    Vector3 closestWalkTarget = walkTargets[0].position;
+
+                    foreach (var item in walkTargets)
+                    {
+                        if (Vector3.Distance(item.position, transform.position) < Vector3.Distance(closestWalkTarget, transform.position))
+                        {
+                            closestWalkTarget = item.position;
+                        }
+                    }
+
+                    targetTrigger = hit.collider.GetComponent<Trigger>();
+
+                    targetPos = closestWalkTarget;
+                    targetPos.z = transform.position.z;
+                }
+                else
+                {
+                    if (walkableArea.OverlapPoint(mouseDownPos))
+                    {
+                        targetPos = mouseDownPos;
+                        targetPos.z = transform.position.z;
+                    }
+                }
+            }
+            CheckFacing();
+        }
+
+        //Liikuta
+        if (transform.position != targetPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, playerSpeed);
+            if (targetTrigger != null)
+            {
+                if (transform.position == targetPos)
+                {
+                    targetTrigger.Interract();
+                    targetPos = transform.position;
+                    targetTrigger = null;
+                }
+            }
+        }
+        SetFacing();
+    }
+
+    private void SetFacing()
+    {
+        Quaternion q;
+        if (facingRight)
+        {
+            q = new Quaternion(0, 0, 0, 0);
+        }
+        else
+        {
+            q = new Quaternion(0, 180, 0, 0);
+        }
+        transform.localRotation = q;
+    }
+
+    private void CheckFacing()
+    {
+        if (transform.position.x < targetPos.x)
+        {
+            facingRight = true;
+        }
+        if (transform.position.x > targetPos.x)
+        {
+            facingRight = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Trigger : MonoBehaviour {
+
+    public TriggerType type;
+    public string Name = "Joku esine";
+    [TextArea(4,100)]
+    public string Info = "Tämä esine näyttää vanhalta";
+
+    void Awake()
+    {
+        if (gameObject.GetComponent<Collider2D>() == null)
+        {
+            gameObject.AddComponent<BoxCollider2D>();
+        }
+        gameObject.tag = "Trigger";
+    }
+
+    public void Interract()
+    {
+        StopCoroutine(ClearTextBox());
+        GameObject.Find("TextBox").GetComponent<Text>().text = "<b>" + Name + "</b>" + "\n" + Info;
+        StartCoroutine(ClearTextBox());
+    }
+
+    IEnumerator ClearTextBox()
+    {
+        yield return new WaitForSeconds(3);
+        GameObject.Find("TextBox").GetComponent<Text>().text = string.Empty;
+    }
+
+    void OnMouseEnter()
+    {
+        transform.localScale = new Vector3(1.05f, 1.05f, 1.05f);
+    }
+    void OnMouseExit()
+    {
+        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+}
